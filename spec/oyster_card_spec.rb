a315ac16bda6a778d19468fcc1bbe7cd27edb235
mikey-roberts <mikey_roberts@outlook.com>
2021-06-01 14:33:38 +0100

    challenge 4 responds to and be kind of tests added

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,7 +1,12 @@
 require 'oyster_card'
 
 describe OysterCard do
-  it 'Test works' do
+  it 'instantiates a class' do
   oyster_card = OysterCard.new
+    expect(oyster_card).to be_kind_of(OysterCard)
+  end
+
+  it 'includes a balance' do
+    expect(oyster_card).to respond_to(:balance)
   end
 end
