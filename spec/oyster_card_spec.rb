1d5eee4be75b62f36c863065d570d4f1cf6de913
mikey-roberts <mikey_roberts@outlook.com>
2021-06-01 14:37:47 +0100

    test to set balance value to 0

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -2,11 +2,17 @@ require 'oyster_card'
 
 describe Oystercard do
   it 'instantiates a class' do
-  oyster_card = Oystercard.new
+    oyster_card = Oystercard.new
     expect(oyster_card).to be_kind_of(Oystercard)
   end
 
   it 'includes a balance' do
+    oyster_card = Oystercard.new
     expect(oyster_card).to respond_to(:balance)
+  end
+
+  it 'incldues a balance of 0' do
+    oyster_card = Oystercard.new
+    expect(oyster_card.balance).to eq(0)
   end
 end
