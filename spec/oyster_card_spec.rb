8579a3d4ae2d8f89086599353903d54b5687a967
mikey-roberts <mikey_roberts@outlook.com>
2021-06-01 15:04:04 +0100

    rspec cleaned up and let added to reduce duplication

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,24 +1,24 @@
 require 'oyster_card'
 
 describe Oystercard do
+  let(:oyster_card) { Oystercard.new }
   it 'instantiates a class' do
-    oyster_card = Oystercard.new
     expect(oyster_card).to be_kind_of(Oystercard)
   end
 
   it 'includes a balance' do
-    oyster_card = Oystercard.new
     expect(oyster_card).to respond_to(:balance)
   end
 
   it 'includes a balance of 0' do
-    oyster_card = Oystercard.new
     expect(oyster_card.balance).to eq(0)
   end
 
-  it 'can be topped up' do
-    oyster_card = Oystercard.new
-    oyster_card.top_up(10)
-    expect(oyster_card.balance).to eq(10)
+  context '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    it 'can be topped up' do
+      oyster_card.top_up(10)
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
   end
 end
